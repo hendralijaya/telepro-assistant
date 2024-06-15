3ab77a165231315f732bb9aa80206045d9ae00db
DasonTiovino <114289111+DasonTio@users.noreply.github.com>
2024-06-15 11:25:28 +0700

    feat: chat features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,38 @@
-'use client';
-import { ResizablePanelGroup, ResizablePanel } from '@/components/ui/resizable';
-import SidebarComponent from '@/components/base/sidebarComponent';
-import prisma from '@/lib/db';
-import { Button } from '@/components/ui/button';
+"use client";
+import { ResizablePanelGroup, ResizablePanel } from "@/components/ui/resizable";
+import SidebarComponent from "@/components/base/sidebarComponent";
+import prisma from "@/lib/db";
+import { Button } from "@/components/ui/button";
 
-import { useQuery } from '@tanstack/react-query';
-import axios, { AxiosResponse } from 'axios';
-import { useState } from 'react';
-import { MessageProps } from './api/openai/route';
-import axiosInstance from '@/lib/axiosInstance';
+import { useQuery } from "@tanstack/react-query";
+import axios, { AxiosResponse } from "axios";
+import { PaperPlaneTilt } from "@phosphor-icons/react";
+import { FormEvent, useState } from "react";
+import { MessageProps } from "./api/openai/route";
+import axiosInstance from "@/lib/axiosInstance";
+import ChatboxComponent from "@/components/module/chatboxComponent";
 
 /// Page
 export default function Home() {
   const [messages, setMessages] = useState<MessageProps[]>([]);
 
   // OpenAI API
-  type createMessageProps = {
+  type CreateMessageProps = {
     content: string;
   };
-  const createMessage = async ({ content }: createMessageProps) => {
-    const response = await axiosInstance.post('/openai', {
+  const createMessage = async ({ content }: CreateMessageProps) => {
+    const response = await axiosInstance.post("/openai", {
       content,
     });
+
+    setMessages(response.data);
+  };
+
+  const formSubmitHandler = (event: FormEvent<HTMLFormElement>) => {
+    const form = new FormData(event.target as HTMLFormElement);
+    const data = Object.fromEntries(form.entries()) as CreateMessageProps;
+
+    createMessage(data);
   };
 
   // TODO: Bug Fixing useQuery error
@@ -32,11 +43,11 @@ export default function Home() {
   //   },
   // });
   const { isFetching, data, error, refetch } = useQuery({
-    queryKey: ['chat-session'],
+    queryKey: ["chat-session"],
     queryFn: async () => {
-      const response = await fetch('/api/chat-sessions');
+      const response = await fetch("/api/chat-sessions");
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       return response.json();
     },
@@ -71,17 +82,46 @@ export default function Home() {
         <SidebarComponent />
       </ResizablePanel>
       <ResizablePanel defaultSize={86}>
-        <main className="flex min-h-screen flex-col items-center  p-24 bg-gray-100">
-          <h1>Hello</h1>
-          <Button
-            onClick={() =>
-              createMessage({
-                content: 'Hello',
-              })
-            }
+        <main className="flex h-[calc(100vh-5rem)] flex-col items-center p-16 bg-gray-100">
+          <ResizablePanelGroup
+            direction="vertical"
+            className="w-full h-full"
           >
-            Hello
-          </Button>
+            <ResizablePanel
+              defaultSize={92}
+              maxSize={92}
+              className="overflow-auto"
+            >
+              {messages.map((m) => (
+                <ChatboxComponent isChatBot={m.role == "assistant"}>
+                  {m.content}
+                </ChatboxComponent>
+              ))}
+            </ResizablePanel>
+            <ResizablePanel
+              defaultSize={8}
+              className="flex items-center"
+            >
+              <form
+                onSubmit={formSubmitHandler}
+                className="w-full flex items-center gap-2"
+              >
+                <div className="border-4 rounded-full w-full p-2">
+                  <input
+                    type="text"
+                    className="border-none bg-transparent w-full"
+                    placeholder="Type or message"
+                  />
+                </div>
+                <button type="submit">
+                  <PaperPlaneTilt
+                    className="fill-blue-400"
+                    weight={"bold"}
+                  />
+                </button>
+              </form>
+            </ResizablePanel>
+          </ResizablePanelGroup>
         </main>
       </ResizablePanel>
     </ResizablePanelGroup>
--- src/components/module/chatboxComponent.tsx
+++ src/components/module/chatboxComponent.tsx
@@ -14,24 +14,31 @@ export default function ChatboxComponent({
   };
 
   return (
-    <section className="flex flex-col gap-2 w-[10rem]">
-      <div
-        onClick={copyText}
-        className={cn({
-          "rounded-tl-xl rounded-r-full": isChatBot,
-          "rounded-l-full rounded tr-xl": !isChatBot,
-          "border-4 p-2 grid grid-cols-[1fr_5fr]": true,
-        })}
-      >
-        <Robot
+    <section
+      className={cn({
+        "flex w-full": true,
+        "justify-end": !isChatBot,
+      })}
+    >
+      <div className="flex flex-col gap-2 w-[30%]">
+        <div
+          onClick={copyText}
           className={cn({
-            hidden: !isChatBot,
+            "rounded-tl-xl rounded-r-full": isChatBot,
+            "rounded-l-full rounded tr-xl": !isChatBot,
+            "border-4 p-2 grid grid-cols-[1fr_5fr]": true,
           })}
-        />
-        <div>{children}</div>
-      </div>
-      <div className="flex">
-        <CopySimple className="rounded-full bg-white p-1" />
+        >
+          <Robot
+            className={cn({
+              hidden: !isChatBot,
+            })}
+          />
+          <div>{children}</div>
+        </div>
+        <div className="flex">
+          <CopySimple className="rounded-full bg-white p-1" />
+        </div>
       </div>
     </section>
   );
